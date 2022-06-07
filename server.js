c845e7cc5e0bc1d0019321cbdcab62149b18caaa
Alina <alinasuv@gmail.com>
2022-06-07 20:11:04 +0300

    done

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+const db = require('./config/db');
 
+db.then(() => {
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
+}).catch(console.error);
+  
 app.get("/");
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-});
